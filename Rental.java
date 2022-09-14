02b94319693793ff182c1c3d6ff12b1eb6134801
gedeaopereira <82479616+gedeaopereira@users.noreply.github.com>
2022-09-14 13:16:16 +0000

    Commit 6

--- Rental.java
+++ Rental.java
@@ -36,5 +36,17 @@ public class Rental {
         }
     return thisAmount;
    }
+
+  public int getFrequentRenterPoints() { 
+      //Adicionar o trecho de código extraído.
+    int frequentRenterPoints = 0;
+    // add frequent renter points
+    frequentRenterPoints ++;
+        // add bonus for a two day new release rental
+    if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+        this.getDaysRented() > 1) frequentRenterPoints ++;
+
+    return frequentRenterPoints;
+    }
   
 }
